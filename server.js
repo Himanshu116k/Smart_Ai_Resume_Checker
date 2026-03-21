2e8a685eba40f79f5ccde257b5cac4d6d0196329
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-21 15:55:29 +0530

    Connecting to DB

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require("./src/app");
+require("dotenv").config();
+const connectionToDB = require("./src/config/database.js");
+
+connectionToDB();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
