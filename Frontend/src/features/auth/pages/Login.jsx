dc7ac74b956bcf47705525205d73145eb9e48123
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-25 20:26:13 +0530

    small work on fontend

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-
+import "../../auth/auth.form.scss"
 const Login = () => {
   return (
-    <div style={{color:"white"}}>
-      Login 
+   <main>
+    <div className="from-container">
+      <h1>Login</h1>
+      <form>
+        <div className="input-group"> 
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" />
+          
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your Password" />
+
+        </div>
+        <button className='button primary-button '>Login</button>
+      </form>
     </div>
+   </main>
   )
 }
 
