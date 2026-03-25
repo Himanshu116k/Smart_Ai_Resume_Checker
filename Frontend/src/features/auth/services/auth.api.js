db897ee8d468e323b90f461f6e4c05e458e459ca
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-25 21:34:03 +0530

    adding two functions

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -8,6 +8,8 @@ export async function register(username,email,password) {
             withCredentials:true
         })
 
+        return responce.data;
+
     } catch (error) {
         console.log(error);
     }
@@ -24,8 +26,37 @@ export async function login(email,password) {
             withCredentialsL:true
         })
 
+        return responce.data;
+
     } catch (error) {
         console.log(error);
     }
     
+}
+
+export async function logut() {
+    
+    try {
+        const responce = await axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/api/auth/logout`,{
+            withCredentials:true
+        })
+        
+        return responce.data;
+    } catch (error) {
+                console.log(error);
+
+    }
+}
+
+export async function getMe(params) {
+
+    try {
+
+        const responce = await axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/api/auth/get-me`,{
+            withCredentials:true
+        })
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
