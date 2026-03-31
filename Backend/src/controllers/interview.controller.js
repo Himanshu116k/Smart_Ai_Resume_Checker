d7bbdb67436fa91c9ede686530fb70df5e0e7c29
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-31 19:46:48 +0530

    small bug fixing

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,22 +1,38 @@
 const pdfParse = require("pdf-parse")
 const generateInterviewReport = require("../services/ai.services")
+const interviewReportModel = require("../models/interviewReport.model.js")
+
+
 
 async function genrateInterViewReportControll(req,res){
 
     
     const resumeFile = req.file
-    const resumeContent = pdfPfarse (req.file.buffer)
+    const resumeContent =  await (new pdfPfarse.PDFParse (req.file.buffer).getText())
     const {selfDescription,jobDescription} = req.body;
 
     const generateInterviewReportByAi = await generateInterviewReport({
         resume:resumeContent,
         selfDescription,
         jobDescription
+
+        
     })
 
+    const interviewReport = await interviewReportModel.create({
+        user:req.user.id,
+        resume:resumeContent,
+        selfDescription,
+        technicalQuestion,
+        ...generateInterviewReportByAi
 
+})
+res.status(201).json({
+    success:true,
+    message:"Interview Report Genrated successfully",
+    interviewReport
+})
 }
-
 
 
 
